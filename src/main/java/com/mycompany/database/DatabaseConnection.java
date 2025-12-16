e008ec1f6455ab44e8ea36d90186fdc8f397dd62
Nguyen Dung <nguyenanhdung831@gmail.com>
2025-12-16 15:01:56 +0700

    Test Connection Database

---
+++ src/main/java/com/mycompany/database/DatabaseConnection.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Nguyen Anh Dung
+ */
+public class DatabaseConnection {
+    public static Connection getConnection(){
+        Connection cons = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            cons = DriverManager.getConnection("jdbc:mysql://localhost:3306/electronics_shop", "root", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return cons;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(getConnection());
+    }
+}
