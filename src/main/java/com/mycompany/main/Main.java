161b866e300c14f934187b68f40b6cedc120c289
huu25102005 <hua25102005@gmail.com>
2025-12-31 20:38:16 +0700

    huu

--- src/main/java/com/mycompany/main/Main.java
+++ src/main/java/com/mycompany/main/Main.java
@@ -11,6 +11,7 @@ package com.mycompany.main;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello dũng!");
     }
+    
 }
